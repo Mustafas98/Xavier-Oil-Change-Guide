ba50a4facb47f7ab33adce6016b090cae57e116b
Mustafas98 <117124491+Mustafas98@users.noreply.github.com>
2023-01-06 00:37:04 +0100

    Create reset-password.php

---
+++ reset-password.php
@@ -0,0 +1,197 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, otherwise redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate new password
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Please enter the new password.";     
+    } elseif(strlen(trim($_POST["new_password"])) < 6){
+        $new_password_err = "Password must have atleast 6 characters.";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm the password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+        
+    // Check input errors before updating the database
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        // Prepare an update statement
+        $sql = "UPDATE users SET password = ? WHERE id = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+            
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                header("location: login.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+ <html>
+<head>
+        <!-- Basic -->
+                        <meta charset="utf-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <!-- Mobile Metas -->
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <!-- Site Metas -->
+            <meta name="keywords" content="" />
+            <meta name="description" content="" />
+         <meta name="author" content="" />
+    
+            <!-- Stylesheet css -->
+            <link href="css/style.css" rel="stylesheet">
+            <!-- fonts css -->
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Raleway:400,700&display=swap" rel="stylesheet" />
+
+            <!--Boot Strap css-->
+            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+            <link href="css/bootsrap.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+
+            <title>Login</title>
+
+        </head>
+        <body>
+            <div class="hero_area">
+        <!-- header section start -->
+        <header class="header_section">
+            <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg custom_nav-container">
+        <a class="navbar-brand" href="index.html">
+          <span>
+            Xavier
+          </span>
+        </a>
+
+        <div class="navbar-collapse" id="">
+          <div class="custom_menu-btn">
+            <button onclick="openNav()">
+              <span class="s-1"> </span>
+              <span class="s-2"> </span>
+              <span class="s-3"> </span>
+            </button>
+          </div>
+          <div id="myNav" class="overlay">
+            <div class="overlay-content">
+            <a href="index.php">HOME</a>
+                <a href="guidelines.php">GUIDELINES</a>
+                <a href="login.php">LOGIN</a>
+                <a href="contact.php">CONTACT US</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+                </div>
+            </header>
+        <!-- end header section -->
+  
+    
+        <section class="vh-100 gradient-custom">
+        <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card bg-dark text-white" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+  
+              <div class="mb-md-5 mt-md-4 pb-5">
+        <h2>Reset Password</h2>
+        <p>Please fill out this form to reset your password.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
+            <div class="form-group">
+                <label>New Password</label>
+                <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
+                <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <a class="btn btn-link ml-2" href="welcome.php">Cancel</a>
+            </div>
+        </form>
+        </div>
+          </div>
+        </div>
+        </div>
+        </div>
+         </section>
+  
+        <!-- footer section Starts -->
+        
+            <section class="container-fluid footer_section">
+                <p>
+                  Design by
+                  <a href="https://herts.ac.uk/">22007515@herts.ac.uk</a>
+                </p>
+              </section>
+              <!-- end footer section -->
+        
+              <!--script start -->
+        
+            
+              <script type="text/javascript" src="css/jscss.js"></script>
+            <script type="text/javascript" src="css/bootstrapcss.js"></script>
+ 
+              
+        
+          <script>
+            function openNav() {
+              document.getElementById("myNav").classList.toggle("menu_width");
+              document
+                .querySelector(".custom_menu-btn")
+                .classList.toggle("menu_btn-style");
+            }
+          </script>
+          <!--Scripts end-->
+        </body>
+    </head>
+</html>
