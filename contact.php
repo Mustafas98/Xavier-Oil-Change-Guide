be619c944d8d8a649cad2a0a7f27878669cee77b
Mustafas98 <117124491+Mustafas98@users.noreply.github.com>
2023-01-08 16:34:58 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,166 +1,127 @@
 <html>
-<head>
-    <!-- Basic -->
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <!-- Mobile Metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <!-- Site Metas -->
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <!-- Stylesheet css -->
-    <link href="css/style.css" rel="stylesheet">
-    <!-- fonts css -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Raleway:400,700&display=swap" rel="stylesheet" />
 
-<!--Boot Strap css-->
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <!-- Stylesheet css -->
+  <link href="css/style.css" rel="stylesheet">
+  <!-- fonts css -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Raleway:400,700&display=swap"
+    rel="stylesheet" />
+
+  <!--Boot Strap css-->
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
   <link href="css/bootsrap.css" rel="stylesheet">
 
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
   <link href="css/responsive.css" rel="stylesheet">
-    <title>GUIDELINES</title>
+  <title>Contact</title>
 
 </head>
+
 <body>
-    <div class="hero_area">
-        <!-- header section start -->
- <header class="header_section">
-    <div class="container-fluid">
-      <nav class="navbar navbar-expand-lg custom_nav-container">
-        <a class="navbar-brand" href="index.html">
-          <span>
-            Xavier
-          </span>
-        </a>
+  <div class="hero_area">
+    <!-- header section start -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              Xavier
+            </span>
+          </a>
 
-        <div class="navbar-collapse" id="">
-          <div class="custom_menu-btn">
-            <button onclick="openNav()">
-              <span class="s-1"> </span>
-              <span class="s-2"> </span>
-              <span class="s-3"> </span>
-            </button>
-          </div>
-          <div id="myNav" class="overlay">
-            <div class="overlay-content">
+          <div class="navbar-collapse" id="">
+            <div class="custom_menu-btn">
+              <button onclick="openNav()">
+                <span class="s-1"> </span>
+                <span class="s-2"> </span>
+                <span class="s-3"> </span>
+              </button>
+            </div>
+            <div id="myNav" class="overlay">
+              <div class="overlay-content">
                 <a href="index.php">HOME</a>
-                <a href="guidelines.php">GUIDELINES</a>
-                <a href="login.php">LOGIN</a>
+                <a href="login.php">GUIDELINES</a>
+
                 <a href="contact.php">CONTACT US</a>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-  </header>
-  <!-- end header section -->
-
-  <section class="vh-100 gradient-custom">
-    <div class="container py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card bg-dark text-white" style="border-radius: 1rem;">
-            <div class="card-body p-5 text-center">
-  
-              <div class="mb-md-5 mt-md-4 pb-5">
-  
-                <h2 class="fw-bold mb-2 text-uppercase">Contact us</h2>
-                <p class="text-white-50 mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
-                  a matter of hours to help you.</p>
-                  <form name="frmContact" id="" frmContact"" method="post"
-            action="" enctype="multipart/form-data"
-            onsubmit="return validateContactForm()">
-
-            <div class="input-row">
-                <label style="padding-top: 20px;">Name</label> <span
-                    id="userName-info" class="info"></span><br /> <input
-                    type="text" class="input-field" name="userName"
-                    id="userName" />
-            </div>
-            <div class="input-row">
-                <label>Email</label> <span id="userEmail-info"
-                    class="info"></span><br /> <input type="text"
-                    class="input-field" name="userEmail" id="userEmail" />
-            </div>
-            <div class="input-row">
-                <label>Subject</label> <span id="subject-info"
-                    class="info"></span><br /> <input type="text"
-                    class="input-field" name="subject" id="subject" />
-            </div>
-            <div class="input-row">
-                <label>Message</label> <span id="userMessage-info"
-                    class="info"></span><br />
-                <textarea name="content" id="content"
-                    class="input-field" cols="60" rows="6"></textarea>
-            </div>
-            <div>
-                <input type="submit" name="send" class="btn-submit"
-                    value="Send" />
-
-                <div id="statusMessage"> 
-                        <?php
-                        if (! empty($message)) {
-                            ?>
-                            <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
-                        <?php
-                        }
-                        ?>
-                    </div>
-            </div>
-        </form>
-  
-                
-  
               </div>
-  
-              
-  
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+
+    <section class="vh-100 gradient-custom">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card bg-dark text-white" style="border-radius: 1rem;">
+              <div class="card-body p-5 text-center">
+
+                <div class="mb-md-5 mt-md-4 pb-5">
+
+                  <h2 class="fw-bold mb-2 text-uppercase">Contact us</h2>
+                  <p class="text-white-50 mb-5">Do you have any questions? Please do not hesitate to contact us
+                    directly. Our team will come back to you within
+                    a matter of hours to help you.</p>
+                  <form action="pro.php" method="POST">
+                    <div class="form-group">
+                      <label>Name:</label>
+                      <input type="text" name="name" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                      <label>Email:</label>
+                      <input type="email" name="email" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                      <label>Message:</label>
+                      <textarea class="form-control" name="message" required></textarea>
+                    </div>
+                    <div class="form-group">
+                      <button class="btn btn-success" type="submit">Submit</button>
+                    </div>
+                  </form>
+
+
+
+                </div>
+
+              </div>
+
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
- 
-
-    <!-- footer section Starts -->
-    <div class="social_container">
-
-      <div class="social-box">
-        <a href="">
-          <img src="images/fb.png" alt="">
-        </a>
-
-        <a href="">
-          <img src="images/twitter.png" alt="">
-        </a>
-        <a href="">
-          <img src="images/linkedin.png" alt="">
-        </a>
-        <a href="">
-          <img src="images/instagram.png" alt="">
-        </a>
-      </div>
-    </div>
   </div>
-</section>
-    <section class="container-fluid footer_section">
-        <p>
-          Design by
-          <a href="https://herts.ac.uk/">22007515@herts.ac.uk</a>
-        </p>
-      </section>
-      <!-- end footer section -->
+  </section>
 
-      <!--script start -->
 
-    
-      <script type="text/javascript" src="css/jscss.js"></script>
-      <script type="text/javascript" src="css/bootstrapcss.js"></script>
-      
+  <!-- footer section Starts -->
+
+  <section class="container-fluid footer_section">
+    <p>
+      Design by
+      <a href="https://herts.ac.uk/">22007515@herts.ac.uk</a>
+    </p>
+  </section>
+  <!-- end footer section -->
+
+  <!--script start -->
+
+
+  <script type="text/javascript" src="css/jscss.js"></script>
+  <script type="text/javascript" src="css/bootstrapcss.js"></script>
+
 
   <script>
     function openNav() {
@@ -169,45 +130,6 @@
         .querySelector(".custom_menu-btn")
         .classList.toggle("menu_btn-style");
     }
-    function validateContactForm() {
-            var valid = true;
-
-            $(".info").html("");
-            $(".input-field").css('border', '#e0dfdf 1px solid');
-            var userName = $("#userName").val();
-            var userEmail = $("#userEmail").val();
-            var subject = $("#subject").val();
-            var content = $("#content").val();
-            
-            if (userName == "") {
-                $("#userName-info").html("Required.");
-                $("#userName").css('border', '#e66262 1px solid');
-                valid = false;
-            }
-            if (userEmail == "") {
-                $("#userEmail-info").html("Required.");
-                $("#userEmail").css('border', '#e66262 1px solid');
-                valid = false;
-            }
-            if (!userEmail.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
-            {
-                $("#userEmail-info").html("Invalid Email Address.");
-                $("#userEmail").css('border', '#e66262 1px solid');
-                valid = false;
-            }
-
-            if (subject == "") {
-                $("#subject-info").html("Required.");
-                $("#subject").css('border', '#e66262 1px solid');
-                valid = false;
-            }
-            if (content == "") {
-                $("#userMessage-info").html("Required.");
-                $("#content").css('border', '#e66262 1px solid');
-                valid = false;
-            }
-            return valid;
-        }
   </script>
   <!--Scripts end-->
 </body>
