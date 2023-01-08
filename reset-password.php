6a3873013164717365d8afb8c70e2550a3c322ca
Mustafas98 <117124491+Mustafas98@users.noreply.github.com>
2023-01-09 00:18:35 +0100

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -103,7 +103,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <header class="header_section">
             <div class="container-fluid">
       <nav class="navbar navbar-expand-lg custom_nav-container">
-        <a class="navbar-brand" href="index.php">
+        <a class="navbar-brand" href="index.html">
           <span>
             Xavier
           </span>
