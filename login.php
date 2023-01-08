5a5786a4e93f296b05f29d22da1568e57a7b93f4
Mustafas98 <117124491+Mustafas98@users.noreply.github.com>
2023-01-08 16:36:51 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,222 +1,228 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
-    exit;
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+  header("location: welcome.php");
+  exit;
 }
- 
+
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-                            
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
-                            // Redirect user to welcome page
-                            header("location: welcome.php");
-                        } else{
-                            // Password is not valid, display a generic error message
-                            $login_err = "Invalid username or password.";
-                        }
-                    }
-                } else{
-                    // Username doesn't exist, display a generic error message
-                    $login_err = "Invalid username or password.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
 
-            // Close statement
-            mysqli_stmt_close($stmt);
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  // Check if username is empty
+  if (empty(trim($_POST["username"]))) {
+    $username_err = "Please enter username.";
+  } else {
+    $username = trim($_POST["username"]);
+  }
+
+  // Check if password is empty
+  if (empty(trim($_POST["password"]))) {
+    $password_err = "Please enter your password.";
+  } else {
+    $password = trim($_POST["password"]);
+  }
+
+  // Validate credentials
+  if (empty($username_err) && empty($password_err)) {
+    // Prepare a select statement
+    $sql = "SELECT id, username, password FROM users WHERE username = ?";
+
+    if ($stmt = mysqli_prepare($link, $sql)) {
+      // Bind variables to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+      // Set parameters
+      $param_username = $username;
+
+      // Attempt to execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+        // Store result
+        mysqli_stmt_store_result($stmt);
+
+        // Check if username exists, if yes then verify password
+        if (mysqli_stmt_num_rows($stmt) == 1) {
+          // Bind result variables
+          mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+          if (mysqli_stmt_fetch($stmt)) {
+            if (password_verify($password, $hashed_password)) {
+              // Password is correct, so start a new session
+              session_start();
+
+              // Store data in session variables
+              $_SESSION["loggedin"] = true;
+              $_SESSION["id"] = $id;
+              $_SESSION["username"] = $username;
+
+              // Redirect user to welcome page
+              header("location: welcome.php");
+            } else {
+              // Password is not valid, display a generic error message
+              $login_err = "Invalid username or password.";
+            }
+          }
+        } else {
+          // Username doesn't exist, display a generic error message
+          $login_err = "Invalid username or password.";
         }
+      } else {
+        echo "Oops! Something went wrong. Please try again later.";
+      }
+
+      // Close statement
+      mysqli_stmt_close($stmt);
     }
-    
-    // Close connection
-    mysqli_close($link);
+  }
+
+  // Close connection
+  mysqli_close($link);
 }
 ?>
 <html>
+
 <head>
-        <!-- Basic -->
-                        <meta charset="utf-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <!-- Mobile Metas -->
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <!-- Site Metas -->
-            <meta name="keywords" content="" />
-            <meta name="description" content="" />
-         <meta name="author" content="" />
-    
-            <!-- Stylesheet css -->
-            <link href="css/style.css" rel="stylesheet">
-            <!-- fonts css -->
-            <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Raleway:400,700&display=swap" rel="stylesheet" />
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
 
-            <!--Boot Strap css-->
-            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
-            <link href="css/bootsrap.css" rel="stylesheet">
-        <link href="css/responsive.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+  <!-- Stylesheet css -->
+  <link href="css/style.css" rel="stylesheet">
+  <!-- fonts css -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Raleway:400,700&display=swap"
+    rel="stylesheet" />
+
+  <!--Boot Strap css-->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootsrap.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
 
-            <title>Login</title>
+  <title>Login</title>
 
-        </head>
-        <body>
-            <div class="hero_area">
-        <!-- header section start -->
-        <header class="header_section">
-            <div class="container-fluid">
-      <nav class="navbar navbar-expand-lg custom_nav-container">
-        <a class="navbar-brand" href="index.html">
-          <span>
-            Xavier
-          </span>
-        </a>
+</head>
 
-        <div class="navbar-collapse" id="">
-          <div class="custom_menu-btn">
-            <button onclick="openNav()">
-              <span class="s-1"> </span>
-              <span class="s-2"> </span>
-              <span class="s-3"> </span>
-            </button>
-          </div>
-          <div id="myNav" class="overlay">
-            <div class="overlay-content">
-            <a href="index.php">HOME</a>
-                <a href="guidelines.php">GUIDELINES</a>
-                <a href="login.php">LOGIN</a>
+<body>
+  <div class="hero_area">
+    <!-- header section start -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              Xavier
+            </span>
+          </a>
+
+          <div class="navbar-collapse" id="">
+            <div class="custom_menu-btn">
+              <button onclick="openNav()">
+                <span class="s-1"> </span>
+                <span class="s-2"> </span>
+                <span class="s-3"> </span>
+              </button>
+            </div>
+            <div id="myNav" class="overlay">
+              <div class="overlay-content">
+                <a href="index.php">HOME</a>
+                <a href="login.php">GUIDELINES</a>
                 <a href="contact.php">CONTACT US</a>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-                </div>
-            </header>
-        <!-- end header section -->
-  
-    
-        <section class="vh-100 gradient-custom">
-        <div class="container py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card bg-dark text-white" style="border-radius: 1rem;">
-            <div class="card-body p-5 text-center">
-  
-              <div class="mb-md-5 mt-md-4 pb-5">
-  
-                <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                <p class="text-white-50 mb-5">Please enter your login and password!</p>
-                <?php 
-        if(!empty($login_err)){
-            echo '<div class="alert alert-danger">' . $login_err . '</div>';
-        }        
-        ?>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
 
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <p>Don't have an account? <a href="singup.php">Sign up now</a>.</p>
-        </form>
-  
+
+    <section class="vh-100 gradient-custom">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card bg-dark text-white" style="border-radius: 1rem;">
+              <div class="card-body p-5 text-center">
+
+                <div class="mb-md-5 mt-md-4 pb-5">
+
+                  <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                  <p class="text-white-50 mb-5">Please enter your login and password for GUIDELINES!</p>
+                  <?php
+                  if (!empty($login_err)) {
+                    echo '<div class="alert alert-danger">' . $login_err . '</div>';
+                  }
+                  ?>
+
+                  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    <div class="form-group">
+                      <label>Username</label>
+                      <input type="text" name="username"
+                        class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+                        value="<?php echo $username; ?>">
+                      <span class="invalid-feedback">
+                        <?php echo $username_err; ?>
+                      </span>
+                    </div>
+                    <div class="form-group">
+                      <label>Password</label>
+                      <input type="password" name="password"
+                        class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                      <span class="invalid-feedback">
+                        <?php echo $password_err; ?>
+                      </span>
+                    </div>
+                    <div class="form-group">
+                      <input type="submit" class="btn btn-primary" value="Login">
+                    </div>
+                    <p>Don't have an account? <a href="singup.php">Sign up now</a>.</p>
+                  </form>
+
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        </div>
-        </div>
-         </section>
-  
-        <!-- footer section Starts -->
-        
-            <section class="container-fluid footer_section">
-                <p>
-                  Design by
-                  <a href="https://herts.ac.uk/">22007515@herts.ac.uk</a>
-                </p>
-              </section>
-              <!-- end footer section -->
-        
-              <!--script start -->
-        
-            
-              <script type="text/javascript" src="css/jscss.js"></script>
-            <script type="text/javascript" src="css/bootstrapcss.js"></script>
- 
-              
-        
-          <script>
-            function openNav() {
-              document.getElementById("myNav").classList.toggle("menu_width");
-              document
-                .querySelector(".custom_menu-btn")
-                .classList.toggle("menu_btn-style");
-            }
-          </script>
-          <!--Scripts end-->
-        </body>
-    </head>
-</html>
+    </section>
+
+<!-- footer section Starts -->
+
+    <section class="container-fluid footer_section">
+        <p>
+          Design by
+          <a href="https://herts.ac.uk/">22007515@herts.ac.uk</a>
+        </p>
+      </section>
+      <!-- end footer section -->
+
+      <!--script start -->
+
+    
+      <script type="text/javascript" src="css/jscss.js"></script>
+      <script type="text/javascript" src="css/bootstrapcss.js"></script>
+      <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/jquery.smartWizard.min.js"></script>
+
+  <script>
+    function openNav() {
+      document.getElementById("myNav").classList.toggle("menu_width");
+      document
+        .querySelector(".custom_menu-btn")
+        .classList.toggle("menu_btn-style");
+    }
+  </script>
+  <!--Scripts end-->
+</body>
