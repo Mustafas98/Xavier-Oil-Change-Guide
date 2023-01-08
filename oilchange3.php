d0a26f6b83e31776939339d65f978a32e37ecb13
Mustafas98 <117124491+Mustafas98@users.noreply.github.com>
2023-01-08 16:37:15 +0100

    Create oilchange3.php

---
+++ oilchange3.php
@@ -0,0 +1,122 @@
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <!-- Stylesheet css -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- fonts css -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Raleway:400,700&display=swap"
+        rel="stylesheet" />
+
+    <!--Boot Strap css-->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootsrap.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+
+    <title>GUIDELINES</title>
+
+</head>
+
+<body>
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <div class="container-fluid">
+                <nav class="navbar navbar-expand-lg custom_nav-container">
+                    <a class="navbar-brand" href="index.html">
+                        <span>
+                            Xavier
+                        </span>
+                    </a>
+
+                    <div class="navbar-collapse" id="">
+                        <div class="custom_menu-btn">
+                            <button onclick="openNav()">
+                                <span class="s-1"> </span>
+                                <span class="s-2"> </span>
+                                <span class="s-3"> </span>
+                            </button>
+                        </div>
+                        <div id="myNav" class="overlay">
+                            <div class="overlay-content">
+                                <a href="index.php">HOME</a>
+                                <a href="login.php">GUIDELINES</a>
+                                <a href="contact.php">CONTACT US</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <!-- end header section -->
+        <section class="vh-100 gradient-custom">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-14 col-md-18 col-lg-10 col-xl-10">
+                        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                            <div class="card-body p-5 text-center">
+
+                                <div class="container-fluid">
+                                Find the oil filter. It's the softball-sized cylindrical component screwed onto the engine (though some engines have a housing that you slip a filter cartridge into). Using your hand or an oil-filter wrench (you'll likely need the latter), loosen the oil filter (turn counterclockwise) enough that the oil starts to come out of the top and drip down into your receptacle
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4"><img class="img-responsive" src="images/filter.jpg">
+                                    </div>
+                                    <div class="col-md-8">.</div>
+                                </div>
+                                <a href="oilchange2.php" class="btn btn-primary">&laquo; Previous</a>
+                                <a href="oilchange4.php" class="btn btn-primary">Next &raquo;</a>
+
+
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- footer section Starts -->
+        <section>
+
+        </section>
+        <section class="container-fluid footer_section">
+            <p>
+                Design & Copyrights by
+                <a href="https://herts.ac.uk/">22007515@herts.ac.uk</a>
+            </p>
+        </section>
+        <!-- end footer section -->
+
+        <!--script start -->
+
+
+        <script type="text/javascript" src="css/jscss.js"></script>
+        <script type="text/javascript" src="css/bootstrapcss.js"></script>
+        <script type="text/javascript"
+            src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/jquery.smartWizard.min.js"></script>
+
+        <script>
+            function openNav() {
+                document.getElementById("myNav").classList.toggle("menu_width");
+                document
+                    .querySelector(".custom_menu-btn")
+                    .classList.toggle("menu_btn-style");
+            }
+        </script>
+        <!--Scripts end-->
+</body>
