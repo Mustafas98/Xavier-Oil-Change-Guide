48e3e04e2418f3a5e991c475d57703e96d74626b
Mustafas98 <117124491+Mustafas98@users.noreply.github.com>
2023-01-08 16:34:54 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,15 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$dbname = "xavier";
+$dbname = "xavier1";
 
 // Create connection
 $link = mysqli_connect($host, $username, $password, $dbname);
+$mysqli = new mysqli($host, $username, $password, $dbname);
+$conn=mysqli_connect($host, $username, $password, $dbname);
+	  
+
+
  
 // Check connection
 if($link === false){
