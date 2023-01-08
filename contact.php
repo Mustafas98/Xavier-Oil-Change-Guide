7896f4a137b7bbcb6b82507890aeff31c06183c5
Mustafas98 <117124491+Mustafas98@users.noreply.github.com>
2023-01-08 09:15:18 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -70,37 +70,47 @@
                 <h2 class="fw-bold mb-2 text-uppercase">Contact us</h2>
                 <p class="text-white-50 mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
                   a matter of hours to help you.</p>
-                <form id="contact-form" name="contact-form" action="mail.php" method="POST"></form>
-                <div class="form-outline form-white mb-4">
-                        <input type="text" id="name" name="name" class="form-control ">
-                          <label for="name" class="">Your name</label>
-          
-                  </div>
-                  <div class="form-outline form-white mb-4">
-                    <input type="text" id="email" name="email" class="form-control">
-                          <label for="email" class="">Your email</label>
-      
-              </div>
-                  <div class="form-outline form-white mb-4">
-                    <input type="text" id="subject" name="subject" class="form-control">
-                    <label for="subject" class="">Subject</label>
-  
-                  </div>
+                  <form name="frmContact" id="" frmContact"" method="post"
+            action="" enctype="multipart/form-data"
+            onsubmit="return validateContactForm()">
 
-                
-                <div class="col-md-12">
+            <div class="input-row">
+                <label style="padding-top: 20px;">Name</label> <span
+                    id="userName-info" class="info"></span><br /> <input
+                    type="text" class="input-field" name="userName"
+                    id="userName" />
+            </div>
+            <div class="input-row">
+                <label>Email</label> <span id="userEmail-info"
+                    class="info"></span><br /> <input type="text"
+                    class="input-field" name="userEmail" id="userEmail" />
+            </div>
+            <div class="input-row">
+                <label>Subject</label> <span id="subject-info"
+                    class="info"></span><br /> <input type="text"
+                    class="input-field" name="subject" id="subject" />
+            </div>
+            <div class="input-row">
+                <label>Message</label> <span id="userMessage-info"
+                    class="info"></span><br />
+                <textarea name="content" id="content"
+                    class="input-field" cols="60" rows="6"></textarea>
+            </div>
+            <div>
+                <input type="submit" name="send" class="btn-submit"
+                    value="Send" />
 
-                  <div class="md-form">
-                      <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                      <label for="message">Your message</label>
-                  </div>
-                  
-                  <button href="#" type="submit" class="btn btn-primary btn-block mb-4">Send</button>
-                  
-                  <div class="d-flex justify-content-between mt-4">
-                      <a class="text-white-50 fw-bold" href="login.php">LOGIN</a>
-                      <a class="text-white-50 fw-bold" href="singup.php">SINGUP</a>
-                  </div>
+                <div id="statusMessage"> 
+                        <?php
+                        if (! empty($message)) {
+                            ?>
+                            <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
+                        <?php
+                        }
+                        ?>
+                    </div>
+            </div>
+        </form>
   
                 
   
@@ -159,6 +169,45 @@
         .querySelector(".custom_menu-btn")
         .classList.toggle("menu_btn-style");
     }
+    function validateContactForm() {
+            var valid = true;
+
+            $(".info").html("");
+            $(".input-field").css('border', '#e0dfdf 1px solid');
+            var userName = $("#userName").val();
+            var userEmail = $("#userEmail").val();
+            var subject = $("#subject").val();
+            var content = $("#content").val();
+            
+            if (userName == "") {
+                $("#userName-info").html("Required.");
+                $("#userName").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (userEmail == "") {
+                $("#userEmail-info").html("Required.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (!userEmail.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
+            {
+                $("#userEmail-info").html("Invalid Email Address.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+
+            if (subject == "") {
+                $("#subject-info").html("Required.");
+                $("#subject").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (content == "") {
+                $("#userMessage-info").html("Required.");
+                $("#content").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            return valid;
+        }
   </script>
   <!--Scripts end-->
 </body>
